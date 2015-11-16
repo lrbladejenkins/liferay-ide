dc04a75dad4fd1848bf8558d1b81d45d7c851590
Gregory Amerson <gregory.amerson@liferay.com>
2015-11-16 07:46:58 -0600

    Upgrade assistant changes for NAS

--- tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/upgrade/CopyPortalSettingsHandler.java
+++ tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/upgrade/CopyPortalSettingsHandler.java
@@ -1,11 +1,11 @@
 package com.liferay.ide.project.ui.upgrade;
 
 import com.liferay.blade.api.Command;
+import com.liferay.blade.api.CommandException;
 import com.liferay.ide.project.core.upgrade.Liferay7UpgradeAssistantSettings;
 import com.liferay.ide.project.core.upgrade.PortalSettings;
 import com.liferay.ide.project.core.upgrade.UpgradeAssistantSettingsUtil;
 import com.liferay.ide.project.ui.ProjectUI;
-import com.liferay.ide.ui.util.UIUtil;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,11 +14,9 @@ import java.util.Map;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.window.Window;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.sapphire.modeling.ProgressMonitor;
-import org.eclipse.sapphire.modeling.Status;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.jobs.Job;
 
 
 /**
@@ -35,111 +33,59 @@ public class CopyPortalSettingsHandler extends AbstractOSGiCommandHandler
     }
 
     /**
-     * This executes on a job/worker thread
-     */
-    public static final Status execute( final GetPortalSettingsOp op, final ProgressMonitor pm )
-    {
-        Status retval = null;
-
-        final Command command = new CopyPortalSettingsHandler().getCommand();
-
-        final File sourceLiferayLocationDir = op.getSourceLiferayLocation().content().toFile();
-        final File destLiferayLocationDir = op.getDestinationLiferayLocation().content().toFile();
-        final String sourceName = op.getSourceLiferayName().content();
-        final String destName = op.getDestinationLiferayName().content();
-
-        final Map<String, File> parameters = new HashMap<>();
-        parameters.put( "source", sourceLiferayLocationDir );
-        parameters.put( "dest", destLiferayLocationDir );
-
-        try
-        {
-            synchronized( ProjectUI.getDefault() )
-            {
-                Liferay7UpgradeAssistantSettings settings =
-                    UpgradeAssistantSettingsUtil.getObjectFromStore( Liferay7UpgradeAssistantSettings.class );
-
-                if( settings == null )
-                {
-                    settings = new Liferay7UpgradeAssistantSettings();
-                }
-
-                PortalSettings portalSettings = settings.getPortalSettings();
-
-                if( portalSettings == null )
-                {
-                    portalSettings = new PortalSettings();
-                }
-
-                portalSettings.setPreviousName( sourceName );
-                portalSettings.setPreviousLiferayPortalLocation( sourceLiferayLocationDir.getPath() );
-                portalSettings.setNewName( destName );
-                portalSettings.setNewLiferayPortalLocation( destLiferayLocationDir.getPath() );
-
-                settings.setPortalSettings( portalSettings );
-
-                try
-                {
-                    UpgradeAssistantSettingsUtil.setObjectToStore( Liferay7UpgradeAssistantSettings.class, settings );
-                }
-                catch( IOException e )
-                {
-                    e.printStackTrace();
-                }
-            }
-
-            command.execute( parameters );
-
-            retval = Status.createOkStatus();
-        }
-        catch( Exception e )
-        {
-            retval = Status.createErrorStatus( e );
-        }
-
-        return retval;
-    }
-
-    /**
      * This executes on UI thread
      * @throws ExecutionException
      */
     @Override
-    protected Object execute( ExecutionEvent event, Command command ) throws ExecutionException
+    protected Object execute( ExecutionEvent event, final Command command ) throws ExecutionException
     {
-        final GetPortalSettingsWizard wizard = new GetPortalSettingsWizard();
-
         try
         {
             Liferay7UpgradeAssistantSettings settings =
                 UpgradeAssistantSettingsUtil.getObjectFromStore( Liferay7UpgradeAssistantSettings.class );
 
+            if( settings == null || settings.getPortalSettings() == null )
+            {
+                // TODO show GetPortalSettingsWizard
+            }
+
             if( settings != null )
             {
                 PortalSettings portalSettings = settings.getPortalSettings();
 
-                wizard.element().setSourceLiferayName( portalSettings.getPreviousName() );
-                wizard.element().setSourceLiferayLocation( portalSettings.getPreviousLiferayPortalLocation() );
-                wizard.element().setDestinationLiferayName( portalSettings.getNewName() );
-                wizard.element().setDestinationLiferayLocation( portalSettings.getNewLiferayPortalLocation() );
+                final File sourceLiferayLocationDir = new File( portalSettings.getPreviousLiferayPortalLocation() );
+                final File destLiferayLocationDir = new File( portalSettings.getNewLiferayPortalLocation() );
+
+                final Map<String, File> parameters = new HashMap<>();
+                parameters.put( "source", sourceLiferayLocationDir );
+                parameters.put( "dest", destLiferayLocationDir );
+
+                new Job("Copy portal settings")
+                {
+                    @Override
+                    protected IStatus run( IProgressMonitor monitor )
+                    {
+                        try
+                        {
+                            command.execute( parameters );
+                        }
+                        catch( CommandException e )
+                        {
+                            return ProjectUI.createErrorStatus( "Command failed", e );
+                        }
+
+                        return org.eclipse.core.runtime.Status.OK_STATUS;
+                    }
+
+                }.schedule();
             }
         }
         catch( IOException e )
         {
+            return e;
         }
 
-        int retval = new WizardDialog( UIUtil.getActiveShell(), wizard ).open();
-
-        if( retval == Window.OK )
-        {
-            MessageDialog.openInformation( UIUtil.getActiveShell(), "Copy Portal Settings", "Copy successful." );
-
-            return null;
-        }
-        else
-        {
-            throw new ExecutionException( "Copy portal settings command failed" );
-        }
+        return null;
     }
 
 }
--- tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/upgrade/GetPortalSettingsWizard.java
+++ tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/upgrade/GetPortalSettingsWizard.java
@@ -1,5 +1,14 @@
 package com.liferay.ide.project.ui.upgrade;
 
+import com.liferay.ide.project.core.upgrade.Liferay7UpgradeAssistantSettings;
+import com.liferay.ide.project.core.upgrade.PortalSettings;
+import com.liferay.ide.project.core.upgrade.UpgradeAssistantSettingsUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.sapphire.modeling.ProgressMonitor;
+import org.eclipse.sapphire.modeling.Status;
 import org.eclipse.sapphire.ui.def.DefinitionLoader;
 import org.eclipse.sapphire.ui.forms.swt.SapphireWizard;
 
@@ -18,5 +27,46 @@ public class GetPortalSettingsWizard extends SapphireWizard<GetPortalSettingsOp>
     private static GetPortalSettingsOp createDefaultOp()
     {
         return GetPortalSettingsOp.TYPE.instantiate();
+    }
+
+    public static final Status execute( final GetPortalSettingsOp op, final ProgressMonitor pm )
+    {
+        final File sourceLiferayLocationDir = op.getSourceLiferayLocation().content().toFile();
+        final File destLiferayLocationDir = op.getDestinationLiferayLocation().content().toFile();
+        final String sourceName = op.getSourceLiferayName().content();
+        final String destName = op.getDestinationLiferayName().content();
+
+        try
+        {
+            Liferay7UpgradeAssistantSettings settings =
+                UpgradeAssistantSettingsUtil.getObjectFromStore( Liferay7UpgradeAssistantSettings.class );
+
+            if( settings == null )
+            {
+                settings = new Liferay7UpgradeAssistantSettings();
+            }
+
+            PortalSettings portalSettings = settings.getPortalSettings();
+
+            if( portalSettings == null )
+            {
+                portalSettings = new PortalSettings();
+            }
+
+            portalSettings.setPreviousName( sourceName );
+            portalSettings.setPreviousLiferayPortalLocation( sourceLiferayLocationDir.getPath() );
+            portalSettings.setNewName( destName );
+            portalSettings.setNewLiferayPortalLocation( destLiferayLocationDir.getPath() );
+
+            settings.setPortalSettings( portalSettings );
+
+            UpgradeAssistantSettingsUtil.setObjectToStore( Liferay7UpgradeAssistantSettings.class, settings );
+        }
+        catch( IOException e )
+        {
+            return Status.createErrorStatus( e );
+        }
+
+        return Status.createOkStatus();
     }
 }
